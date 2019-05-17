707c63fadde66cf73b8678a40253c55f2daac2f4
Amanda <amandamojabi@gmail.com>
2019-05-16 20:02:00 -0700

    assignment 11 update

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    const duration = player.getDuration();
-    $('#time-control .total-time').text( duration );
+    const totalTime = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text( totalTime );
   }
 }
 const helper = new Helper();
